39a370a3a8a0413293c80f70a52dc867fb2bb9aa
Sakshi Goel <sakshigoel@192.168.1.4>
2022-05-11 07:00:15 +0530

    add user_sign_in user_sign_up inside views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 // controller function for route '/'
 module.exports.home = function(req, res){
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
     return res.render('home', {
         title: "Home"
     });

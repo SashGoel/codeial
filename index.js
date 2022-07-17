a644fc7fc3fef3ae5ad571aae575b2ed1ea45178
System Administrator <root@Sakshis-MacBook-Pro.local>
2022-07-18 01:19:54 +0530

    Remove Chat server

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const customMware = require('./config/middleware')
 // setup the chat server to be used with socket.io
 const chatServer = require('http').Server(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
+// chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 const path = require('path');
 
